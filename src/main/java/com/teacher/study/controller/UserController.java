7bf91eae20ea632ca910c2bdc104c81f4718a728
wzh <837713748@qq.com>
2020-02-02 07:05:32 +0800

    aaaa

--- src/main/java/com/teacher/study/controller/UserController.java
+++ src/main/java/com/teacher/study/controller/UserController.java
@@ -122,8 +122,8 @@ public class UserController {
                 user.setAcc(Base.encode(user.getAcc()));
                 user.setPwd(Base.encode(user.getPwd()));
                 User login = userService.login(user);
-                user.setAcc(Base.toencode(user.getAcc()));
-                user.setPwd(Base.toencode(user.getPwd()));
+                user.setAcc(Base.toencode(login.getAcc()));
+                user.setPwd(Base.toencode(login.getPwd()));
                 if(null==login.getId()){
                     return new Return().custom("没有此账号");
                 }
